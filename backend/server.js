ca14e18cb51449fab1bfe6bc12b2b4b7771b42e5
rachana26-spec <nbrachana50@gmail.com>
2025-06-23 09:57:25 +0530

    Server Error

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use("/api/testimonials", require("./routes/testimonials"));
 app.use("/api/cart", require("./routes/cart"));
 app.use("/api", require("./routes/index"));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
