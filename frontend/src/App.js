a4784c1c9ed6da42ee7d1526ea0cff79e78deaa2
rachana26-spec <nbrachana50@gmail.com>
2025-06-25 18:27:42 +0530

    Users deletion

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  useLocation,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Cart from "./pages/Cart";
@@ -37,68 +32,9 @@ import CustomOrderSummary from "./pages/CustomOrderSummary";
 import Search from "./pages/Search";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
-import Sidebar from "./components/Sidebar";
 import Layout from "./components/Layout";
 import AdminFeedback from "./pages/AdminFeedback";
 import AdminUserManagement from "./pages/AdminUserManagement";
-
-function AppContent() {
-  const location = useLocation();
-  const isAdminPage = location.pathname.startsWith("/admin");
-  return (
-    <div className={`App ${isAdminPage ? "admin-bg" : ""}`}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/preorder" element={<PreOrder />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/admin-signup" element={<AdminSignup />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/payment" element={<Payment />} />
-        <Route path="/thank-you" element={<ThankYou />} />
-        <Route path="/my-orders" element={<MyOrders />} />
-        <Route path="/payment-options" element={<PaymentOptions />} />
-        <Route
-          path="/payment-method-selection"
-          element={<PaymentMethodSelection />}
-        />
-        <Route path="/bank-transfer" element={<BankTransfer />} />
-        <Route path="/preorder-payment" element={<PreOrderPayment />} />
-        <Route
-          path="/preorder-payment-options"
-          element={<PreOrderDepositPayment />}
-        />
-        <Route
-          path="/preorder-deposit-payment"
-          element={<PreOrderDepositPayment />}
-        />
-        <Route
-          path="/preorder-payment-card"
-          element={<PreOrderPaymentCard />}
-        />
-        <Route
-          path="/preorder-payment-bank"
-          element={<PreOrderPaymentBank />}
-        />
-        <Route path="/about" element={<About />} />
-        <Route path="/custom-order-summary" element={<CustomOrderSummary />} />
-        <Route path="/search" element={<Search />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/admin" element={<Admin />} />
-        <Route path="/admin/products" element={<AdminProducts />} />
-        <Route path="/admin/orders" element={<AdminOrders />} />
-        <Route path="/admin/preorders" element={<AdminPreOrders />} />
-        <Route path="/admin/feedback" element={<AdminFeedback />} />
-        <Route path="/admin/users" element={<AdminUserManagement />} />
-      </Routes>
-      <Sidebar />
-    </div>
-  );
-}
 
 function App() {
   return (
